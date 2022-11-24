7f5e5c419c27537aeae38950b49c55c214b6dc06
saim-hossain <saimhossain023@gmail.com>
2022-11-25 04:04:00 +0600

    phone category added

--- index.js
+++ index.js
@@ -19,6 +19,38 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@aster.gyayqwe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const iphoneCollection = client.db('phonePlanet').collection('iphone')
+        const xiaomiCollection = client.db('phonePlanet').collection('xiaomi')
+        const oneplusCollection = client.db('phonePlanet').collection('oneplus')
+
+        app.get('/iphone', async (req, res) => {
+            const query = {};
+            const iphone = await iphoneCollection.find(query).toArray();
+            res.send(iphone)
+        })
+
+
+        app.get('/xiaomi', async (req, res) => {
+            const query = {};
+            const xiaomi = await xiaomiCollection.find(query).toArray();
+            res.send(xiaomi)
+        })
+
+        app.get('/oneplus', async (req, res) => {
+            const query = {};
+            const oneplus = await oneplusCollection.find(query).toArray();
+            res.send(oneplus)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(() => console.log())
+
 
 
 
