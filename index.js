e001c36d7c9ff3970d061bdc4c1ce81443209c1a
saim-hossain <saimhossain023@gmail.com>
2022-11-26 09:40:56 +0600

    booking query  added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express');
 const app = express();
 
@@ -24,11 +24,56 @@ async function run() {
         const iphoneCollection = client.db('phonePlanet').collection('iphone')
         const xiaomiCollection = client.db('phonePlanet').collection('xiaomi')
         const oneplusCollection = client.db('phonePlanet').collection('oneplus')
+        const usersCollection = client.db('phonePlanet').collection('users')
+        const bookingsCollection = client.db('phonePlanet').collection('bookings')
+
+
+        // jwt
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '365d' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
+        })
+
+        // Users
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
 
         app.get('/iphone', async (req, res) => {
             const query = {};
             const iphone = await iphoneCollection.find(query).toArray();
             res.send(iphone)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+
+            const query = {
+                email: booking.email,
+                item: booking.item,
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already booked this item`
+                return res.send({
+                    acknowledged: false, message
+                })
+            }
+
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
         })
 
 
